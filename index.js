565487d55aaab3b4814643fe399cd4512fb335fd
unknown <giotnhaduh@gmail.com>
2023-03-06 23:06:18 +0700

    adding stripe for payment

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
-const productRoute = require("./routes/product")
-const cartRoute = require("./routes/cart")
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 // const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -15,6 +17,7 @@ mongoose
   .then(() => console.log(`db connected`))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
@@ -22,6 +25,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/payment", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () =>
   console.log(`server on http://localhost:5000`)
